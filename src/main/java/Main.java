b27e154b185e911856047772af26ad4d4ea09205
Dmitriy <dmed832@yandex.ru>
2023-12-22 23:54:04 +0300

    fixed (if value is null)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,19 +10,20 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         List<City> cityList = new ArrayList<>();
         try (Scanner scanner = new Scanner(new File("src/main/resources/cities.csv"))) {
-            scanner.useDelimiter("\\r\\n");   //delimiter
             while (scanner.hasNextLine()) {
                 String string = scanner.nextLine();
-                String[] strings = string.split(";");
+                String[] stringsSplited = string.split(";");
+                String[] strings = Arrays.copyOf(stringsSplited, 6);
+                for (int i = 0; i < 6; i++) {
+                    if (strings[i] == null) {
+                        strings[i] = "";
+                    }
+                }
                 cityList.add(new City(strings[1], strings[2], strings[3], Integer.parseInt(strings[4]), strings[5]));
             }
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getStackTrace());
         }
         for (City city : cityList) {
             System.out.println(city);
         }
-
     }
 }
